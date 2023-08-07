8b25b9f45ad3fb568bb314a3e51ec3b5d9b3e084
f <snake386@hotmail.com>
2023-08-06 20:43:07 -0400

    refact git package

--- internal/git/git.go
+++ internal/git/git.go
@@ -15,21 +15,35 @@ func Pull(path string) (string, error) {
 	return execCommand(path, args...)
 }
 
-func execCommand(path string, args ...string) (string, error) {
-	cmd := exec.Command(args[0], args[1:]...)
-	cmd.Dir = path
-	out, err := cmd.CombinedOutput()
+func Push(path string, origin string, branch string) (string, error) {
+	args := buildCmdArgs("git", "push", origin, branch)
+	return execCommand(path, args...)
+}
 
-	if err != nil {
-		return "", err
-	}
-	return string(out), nil
+func StageAllModified(path string) (string, error) {
+	args := buildCmdArgs("git", "add", "-u")
+	return execCommand(path, args...)
+}
+
+func Commit(path string, message string) (string, error) {
+	args := buildCmdArgs("git", "commit", "-m", message)
+	return execCommand(path, args...)
 }
 
 func buildCmdArgs(args ...string) []string {
 	if runtime.GOOS == "windows" {
 		return append([]string{"cmd", "/C"}, args...)
 	}
-
 	return args
+}
+
+func execCommand(path string, args ...string) (string, error) {
+	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Dir = path
+	out, err := cmd.CombinedOutput()
+
+	if err != nil {
+		return string(out), err
+	}
+	return string(out), nil
 }
