25b2cc63bf44fb34e6d9b5262514542c1e350c07
f <snake386@hotmail.com>
2023-08-10 11:47:49 -0400

    added ErrorMsg custom type

---
+++ internal/error.go
@@ -0,0 +1,17 @@
+package internal
+
+import "fmt"
+
+type ErrorMsg string
+
+const (
+	EmptyError ErrorMsg = ""
+)
+
+func (e ErrorMsg) ToString() string {
+	return string(e)
+}
+
+func (e ErrorMsg) Sprintf(args ...interface{}) ErrorMsg {
+	return ErrorMsg(fmt.Sprintf(e.ToString(), args...))
+}
