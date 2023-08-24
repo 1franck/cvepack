ea6bfd6fe3e3526010371b89057e3725996a9a47
f <snake386@hotmail.com>
2023-08-24 18:44:22 -0400

    simplify

--- internal/ecosystem/packagist/detection.go
+++ internal/ecosystem/packagist/detection.go
@@ -1,14 +1,14 @@
 package packagist
 
 import (
-    "github.com/1franck/cvepack/internal/common"
-    "path/filepath"
+	"github.com/1franck/cvepack/internal/common"
+	"path/filepath"
 )
 
 func DetectComposerJson(path string) bool {
-    return common.FileExists(filepath.Join(path, ComposerFile))
+	return common.FileExists(filepath.Join(path, ComposerFile))
 }
 
 func DetectComposerLock(path string) bool {
-    return common.FileExists(filepath.Join(path, ComposerLockFile))
+	return common.FileExists(filepath.Join(path, ComposerLockFile))
 }
