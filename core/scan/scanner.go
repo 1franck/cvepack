2ae6cd9534b90ab68c06710455cde83bf9b91c38
f <snake386@hotmail.com>
2024-01-25 22:07:36 -0500

    simplify scanner

--- core/scan/scanner.go
+++ core/scan/scanner.go
@@ -1,34 +1,19 @@
 package scan
 
 import (
-	"cvepack/core/ecosystem"
+	es "cvepack/core/ecosystem"
 	"cvepack/core/scan/files"
 	"cvepack/core/scan/github"
-	"fmt"
 )
 
-func Inspect(source ecosystem.Source) *ScannerResult {
-	s := newScanner()
-	return s.Scan(source)
-}
-
-type scanner struct {
-}
-
-func newScanner() *scanner {
-	return &scanner{}
-}
-
-// Scan scans a source for projects
-// support url and path sources
-func (s *scanner) Scan(source ecosystem.Source) *ScannerResult {
+func Inspect(source es.Source) *ScannerResult {
 	scanResult := NewScannerResult(source)
 	defer scanResult.End()
 
 	switch source.Type() {
-	case ecosystem.UrlSource:
-		scanResult.Projects = s.scanUrl(source)
-	case ecosystem.PathSource:
+	case es.UrlSource:
+		scanResult.Projects = inspectUrl(source)
+	case es.PathSource:
 		fileProvider := files.NewProvider(source)
 		scanResult.Projects = ProviderScanner(fileProvider)
 	}
@@ -36,10 +21,8 @@ func (s *scanner) Scan(source ecosystem.Source) *ScannerResult {
 	return scanResult
 }
 
-// scanUrl scan url for projects
-func (s *scanner) scanUrl(source ecosystem.Source) []ecosystem.Project {
-	fmt.Printf("Url %s\n", source.Value)
-	projects := make([]ecosystem.Project, 0)
+func inspectUrl(source es.Source) []es.Project {
+	projects := make([]es.Project, 0)
 	if github.DetectGithubRepoUrl(source) {
 		provider := github.NewProvider(source)
 		return ProviderScanner(provider)
